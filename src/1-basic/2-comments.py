9c522f98ba8df66be800e704f51684aa2b100745
HvTung04 <natsuthcscb@gmail.com>
2026-06-02 11:10:18 +0700

    lesson 1.2: Comments

---
+++ src/1-basic/2-comments.py
@@ -0,0 +1,12 @@
+# Dòng này sẽ được bỏ qua
+"""
+Dòng này sẽ được bỏ qua
+Dòng này sẽ được bỏ qua
+Dòng này sẽ được bỏ qua
+"""
+print("Dòng này sẽ được in ra")
+'''
+Dòng này sẽ được bỏ qua
+Dòng này sẽ được bỏ qua
+Dòng này sẽ được bỏ qua
+'''
